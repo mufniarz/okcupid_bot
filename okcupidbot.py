41692ae847eaaadc169936e3f799faf3228b296c
narottambisht <narottam.singh@jellyfishtechnologies.com>
2020-06-11 00:24:27 +0530

    fixed the exceptions to restart sending intros on error

--- okcupidbot.py
+++ okcupidbot.py
@@ -7,7 +7,7 @@ class okcupidbot:
         print('WAITING FOR BROWSER PROCESS TO COMPLETE...')
         self.driver = webdriver.Chrome()
         self.driver.get('http://okcupid.com')
-        time.sleep(15)
+        time.sleep(10)
         self.login()
 
     def login(self):
@@ -24,30 +24,33 @@ class okcupidbot:
         self.start_intro()
         
     def start_intro(self):
-        print('HOW MANY INTROS TO SEND-->')
-        number_of_intros_to_send = int(input())
-        for i in range(1, number_of_intros_to_send + 1):
-            person_name = self.driver.find_element_by_xpath('//*[@id="quickmatch-wrapper"]/div/div/span/div/div[2]/div/div[2]/span/div/div/div/div[1]/div[1]/div[1]').text
-            self.driver.find_element_by_xpath('//*[@id="quickmatch-wrapper"]/div/div/span/div/div[2]/div/div[2]/span/div/div/div/div[1]/div[1]/div[5]/a').click()
-            time.sleep(10)
-            self.driver.find_element_by_xpath('/html/body/div[1]/main/div[1]/div[2]/div/div/div[3]/span/div/button[2]').click()
-            time.sleep(5)
-            self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[2]/textarea').send_keys('Hey ' + person_name + intro_message)
-            self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[3]/button').click()
-            time.sleep(5)
-            self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[1]/button').click()
-            time.sleep(10)
-            self.driver.find_element_by_xpath('//*[@id="navigation"]/div/span[1]/a[1]').click()
-            time.sleep(10)
-            self.driver.refresh()
-            time.sleep(10)
-            print('SENT INTRO TO -', person_name)
+        try:
+            for i in range(1, 100 + 1):
+                person_name = self.driver.find_element_by_xpath('//*[@id="quickmatch-wrapper"]/div/div/span/div/div[2]/div/div[2]/span/div/div/div/div[1]/div[1]/div[1]').text
+                self.driver.find_element_by_xpath('//*[@id="quickmatch-wrapper"]/div/div/span/div/div[2]/div/div[2]/span/div/div/div/div[1]/div[1]/div[5]/a').click()
+                time.sleep(10)
+                self.driver.find_element_by_xpath('/html/body/div[1]/main/div[1]/div[2]/div/div/div[3]/span/div/button[2]').click()
+                time.sleep(7)
+                self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[2]/textarea').send_keys('Hey ' + person_name + intro_message)
+                self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[3]/button').click()
+                time.sleep(5)
+                self.driver.find_element_by_xpath('//*[@id="main_content"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[1]/button').click()
+                time.sleep(5)
+                self.driver.find_element_by_xpath('//*[@id="navigation"]/div/span[1]/a[1]').click()
+                time.sleep(5)
+                self.driver.refresh()
+                time.sleep(10)
+                print('SENT INTRO TO -', person_name)
 
-        print('ALL ' + str(number_of_intros_to_send) + ' INTROS SENT SUCCESSFULLY DO YOU WISH TO SEND MORE TYPE (y)ES OR (n)O:')
-        user_response = input().lower()
-        if user_response == 'y':
+            user_response = input().lower()
+            if user_response == 'y':
+                self.start_intro()
+            else:
+                print('INVALID RESPONSE')
+        except:
+            print('ERROR ENCOUNTERED, RESTARTING INTRO FUNCTION')
+            self.driver.get('http://okcupid.com')
+            time.sleep(10)
             self.start_intro()
-        else:
-            print('INVALID RESPONSE')
 
 obj = okcupidbot()
